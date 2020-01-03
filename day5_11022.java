785765c4babeff3988ab2faa85a2889d61af5f58
tjfruddnjs1 <tjfruddnjs12@naver.com>
2020-01-03 19:36:01 +0900

    Day 5

---
+++ day5_11022.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class day5_11022  {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine().trim());
+		
+		for(int i=1;i<=n;i++) {
+			String text = br.readLine();
+			String[] word = text.split(" ");
+			
+			int a = Integer.parseInt(word[0]);
+			int b = Integer.parseInt(word[1]);
+			bw.write("Case #"+i+": "+a+" + "+b+" = "+(a+b)+"\n");
+
+		}
+		bw.flush();
+		bw.close();
+				
+	}
+
+}

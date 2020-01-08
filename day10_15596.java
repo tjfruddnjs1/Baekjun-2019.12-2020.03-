366a6e2585729e6a7c812501a7586cf0ad5e1d35
tjfruddnjs1 <tjfruddnjs12@naver.com>
2020-01-08 19:37:49 +0900

    Day 10

---
+++ day10_15596.java
@@ -0,0 +1,20 @@
+package day10_15596;
+
+public class day10_15596 {
+	
+	public class Test {
+	    long sum(int[] a) {
+	        long ans = 0;
+	        for(int i=0;i<a.length;i++){
+	            ans += a[i];
+	        }
+	        return ans;
+	    }
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
---
+++ day10_4673.java
@@ -0,0 +1,10 @@
+package day10_15596;
+
+public class day10_4673 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

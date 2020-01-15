14c59bfc018833d8b79e719db4ec472a254bf199
tjfruddnjs1 <41010744+tjfruddnjs1@users.noreply.github.com>
2020-01-16 03:19:23 +0900

    Day 18[president of association]

--- day18_2775.java
+++ day18_2775.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
 
 public class day18_2775 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		int[] numFloor = new int[N];
+		int[] numUnit = new int[N];
+		int max = 0;
 
+		for (int i = 0; i < N; i++) {
+			numFloor[i] = scan.nextInt();
+			numUnit[i] = scan.nextInt();
+			System.out.println(numberArray(numFloor[i],numUnit[i]));
+		}
+		
 	}
-
+	private static int numberArray(int numFloor,int numUnit) {
+		if(numFloor == 0) return numUnit;
+		else if (numUnit == 0) return 0;
+		else return numberArray(numFloor,numUnit-1) + 
+				numberArray(numFloor-1,numUnit);
+	}
 }
+

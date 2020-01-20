742d8e058803fb25aee78bf584b037bb6320f57e
tjfruddnjs1 <41010744+tjfruddnjs1@users.noreply.github.com>
2020-01-21 00:38:09 +0900

    Update day23_3053.java

--- day23_3053.java
+++ day23_3053.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
 
 public class day23_3053 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		
+		int r = scan.nextInt();
+		
+		String result1 = String.format("%.6f", Math.PI * r * r);
+		String result2 = String.format("%.6f",(double) 2 * r * r);
+		
+		System.out.println(result1);
+		System.out.println(result2);
 	}
 
 }
+

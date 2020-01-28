029a82073bc1068b32b1c00438619acab4f8dc76
tjfruddnjs1 <tjfruddnjs12@naver.com>
2020-01-29 03:52:26 +0900

    Day 31[하나의 숫자에 각 자리수 정렬, 내림차순]

---
+++ day31_1427.java
@@ -0,0 +1,28 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class day31_1427 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+		int input = Integer.parseInt(br.readLine().trim());
+		List<Integer> list = new ArrayList<>();
+		while(input/10 != 0) {
+			list.add(input%10);
+			input /= 10;
+		}	
+		list.add(input);
+		Collections.sort(list, Collections.reverseOrder());
+		
+		for(int i:list) {
+			System.out.print(i);
+		}
+	}
+
+}

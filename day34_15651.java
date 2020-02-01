d4b3b7c60495fc27049f2bd5a8f64a8883be4ca9
tjfruddnjs1 <tjfruddnjs12@naver.com>
2020-02-01 15:23:05 +0900

    Day 34[백트래킹3, 중복처리X, visited[i] X]

---
+++ day34_15651.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class day34_15651 {		
+		private static int[] num;
+		private static StringBuilder sb =new StringBuilder();
+		private static int N,M;
+		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		N = scan.nextInt();
+		M = scan.nextInt();
+		num = new int[N+1];
+		backtrack(0);
+		System.out.print(sb);
+	}
+	private static void backtrack(int count) { 
+		if(count == M) {
+			for(int i=0;i<M;i++) sb.append(num[i]+" ");
+			sb.append("\n");
+			return;
+		}
+		for(int i=1;i<=N;i++) {
+				num[count] = i;
+				backtrack(count+1);
+		}
+	}
+}

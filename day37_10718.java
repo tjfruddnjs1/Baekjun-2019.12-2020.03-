6925768be80e7daa7dc5660718adc618630b135c
tjfruddnjs1 <tjfruddnjs12@naver.com>
2020-02-04 19:44:00 +0900

    Day 37 [basic I/O]

---
+++ day37_10718.java
@@ -0,0 +1,10 @@
+public class day37_10718{
+	
+    public static void main(String[] args){
+    	
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+        
+    }
+    
+}

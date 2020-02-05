7e0a4b36674de17e524cb4af55dd136ee7f6876d
tjfruddnjs1 <tjfruddnjs12@naver.com>
2020-02-05 14:39:24 +0900

    Day 38 [basic I/O, \ → \\, cat]

---
+++ day38_10171.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class day38_10171 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("\\    /\\");
+	     System.out.println(" )  ( ')");
+	     System.out.println("(  /  )");
+	     System.out.println(" \\(__)|");
+	}
+
+}

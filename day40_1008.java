bf6a4ed269e0140cc20a6251c09595be6e6afceb
tjfruddnjs1 <tjfruddnjs12@naver.com>
2020-02-07 20:20:05 +0900

    Day 40 [basic I/O, a/b]

---
+++ day40_1008.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class day40_1008 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		double[] input = new double[2];
+		input[0] = scan.nextDouble();
+		input[1] = scan.nextDouble();
+		
+		System.out.println(input[0]/input[1]);
+	}
+
+}

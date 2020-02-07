73055db776848f24f704217adcd3e49abd78da3e
tjfruddnjs1 <tjfruddnjs12@naver.com>
2020-02-07 20:19:38 +0900

    Day 40 [basic I/O, a*b]

---
+++ day40_10998.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class day40_10998 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int[] input = new int[2];
+		input[0] = scan.nextInt();
+		input[1] = scan.nextInt();
+		System.out.println(input[0] * input[1]);
+	}
+}

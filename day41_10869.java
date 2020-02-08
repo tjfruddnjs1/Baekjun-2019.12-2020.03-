ef1ef7e640756fed101defd6fe684408ac2fa9c8
tjfruddnjs1 <tjfruddnjs12@naver.com>
2020-02-08 12:56:30 +0900

    Day 41 [Basic I/O, 사칙연산 출력]

---
+++ day41_10869.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class day41_10869 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int first = scan.nextInt();
+		int second = scan.nextInt();
+		
+		System.out.println(first+second);
+		System.out.println(first-second);
+		System.out.println(first*second);
+		System.out.println(first/second);
+		System.out.println(first%second);
+	}
+
+}

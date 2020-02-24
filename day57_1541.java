40189b7999128c1a18bcc042f005671a8e5a94bf
tjfruddnjs1 <41010744+tjfruddnjs1@users.noreply.github.com>
2020-02-24 21:53:04 +0900

    Day 57 [그리디 알고리즘, 주어진 식의 괄호를 이용한 최저 합(+,-사용)]

--- day57_1541.java
+++ day57_1541.java
@@ -1,9 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class day57_1541 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) throws IOException {
 
-	}
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        String input = br.readLine();
+        String[] strArr1 = input.split("\\-");
+
+        int minSum = 0;
+        for (int i = 0; i < strArr1.length; i++) {
+
+            String[] strArr2 = strArr1[i].split("\\+");
+
+            int tempSum = 0;
+            for (String x : strArr2) {
+
+                tempSum += Integer.parseInt(x);
+            }
+
+            if (i == 0) tempSum *= -1;
+
+            minSum -= tempSum;
+        }
+
+        System.out.println(minSum);
+    }
 }

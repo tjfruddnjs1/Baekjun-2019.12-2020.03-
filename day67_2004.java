abf45f885e8dde3ce106297d7630216b1602c799
tjfruddnjs1 <tjfruddnjs12@naver.com>
2020-03-06 08:18:19 +0900

    Day 67 [수학3, 위의 문제에서 팩토리얼이 아닌 조합(Combination)이었을 상황]

---
+++ day67_2004.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class day67_2004 {
+
+	public static void main(String args[]) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		long n = sc.nextLong();
+		long m = sc.nextLong();
+		long two = 0, five = 0;
+		
+		for (long i = 2; i <= n; i *= 2) {
+			two += n / i;
+		}
+		for (long i = 2; i <= n - m; i *= 2) {
+			two -= (n - m) / i;
+		}
+		for (long i = 2; i <= m; i *= 2) {
+			two -= m / i;
+		}
+		for (long i = 5; i <= n; i *= 5) {
+			five += n / i;
+		}
+		for (long i = 5; i <= n - m; i *= 5) {
+			five -= (n - m) / i;
+		}
+		for (long i = 5; i <= m; i *= 5) {
+			five -= m / i;
+		}
+		
+		System.out.println(Math.min(two, five));
+	}
+}
